a70854184762738a3497234667979cd94762ef3e
Ooyo <916881735@qq.com>
2020-07-15 18:21:03 +0800

    seller 2

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,9 @@
-// import Vue from "vue";
-// import VueRouter from "vue-router";
-// import routes from "routes/routes.js";
-// Vue.use(VueRouter);
-//
-// export default  new VueRouter({
-//     routes
-//
-// })
+import Vue from "vue";
+import VueRouter from "vue-router";
+import routes from "routes/routes.js";
+Vue.use(VueRouter);
+
+export default  new VueRouter({
+    routes
+
+})
